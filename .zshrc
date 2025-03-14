b888078aaa072819015fbd3bcd10a2a0093032ec
basedceo <99623735+basedceo@users.noreply.github.com>
2025-03-14 16:01:23 -0400

    Add files via upload

---
+++ .zshrc
@@ -0,0 +1,53 @@
+#
+# ~/.zshrc
+#
+
+#fixes URL's for yt-dlp and other stuff
+autoload -Uz bracketed-paste-magic
+zle -N bracketed-paste bracketed-paste-magic
+
+autoload -Uz url-quote-magic
+zle -N self-insert url-quote-magic
+
+#needed for asdf
+. /opt/asdf-vm/asdf.sh
+
+#path updates
+path+=('/home/kenny/.cargo/bin')
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+alias ls='ls --color=auto'
+alias zed='~/.local/bin/zed'
+alias lc='eza'
+# alias to reload xresources colors in st
+alias rel="xrdb merge ~/.Xresources && kill -USR1 $(pidof st)"
+alias vim="nvim"
+alias signal="signal-desktop --password-store=\"gnome-libsecret\" disown"
+alias yt-dlp="/usr/bin/yt-dlp_linux"
+#PS1='[\u@\h \W]\$ '
+#PROMPT="%n@%m:~$"
+#PROMPT='[\u@\h \W]\$ '
+export PS1="%{$(tput setaf 33)%}%n%{$(tput setaf 220)%}@%{$(tput setaf 10)%}%m %{$(tput setaf 13)%}%1~ %{$(tput sgr0)%}$ "
+export ANTHROPIC_API_KEY='API-KEY'
+export OPENAI_API_KEY='API-KEY'
+export GEMINI_API_KEY='API-KEY'
+
+#history file for zsh
+HISTFILE=~/.zsh_history
+
+#how many commands can be saved to history
+HISTSIZE=1000000
+SAVEHIST=1000000
+HISTFILESIZE=
+
+# Share history in every terminal session
+setopt SHARE_HISTORY
+
+set -o vi
+
+export PATH
+
+# alias ls='ls --color=auto'
+# alias grep='grep --color=auto'
+# PS1='[\u@\h \W]\$ '
---
+++ init.lua
@@ -0,0 +1,132 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+   	opts = {
+   		ensure_installed = {
+   			"lua-language-server", "stylua",
+   			"html-lsp", "css-lsp" , "prettier", "rust-analyzer",
+   		},
+    },
+  },
+
+  {
+  "debugloop/telescope-undo.nvim",
+  dependencies = { -- note how they're inverted to above example
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+    },
+  },
+  keys = {
+    { -- lazy style key map
+      "<leader>u",
+      "<cmd>Telescope undo<cr>",
+      desc = "undo history",
+    },
+  },
+  opts = {
+    -- don't use `defaults = { }` here, do this in the main telescope spec
+    extensions = {
+      undo = {
+        -- telescope-undo.nvim config, see below
+      },
+      -- no other extensions here, they can have their own spec too
+    },
+  },
+  config = function(_, opts)
+    -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+    -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+    -- defaults, as well as each extension).
+    require("telescope").setup(opts)
+    require("telescope").load_extension("undo")
+  end,
+},
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css"
+      },
+    },
+  },
+
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    ["rust-analyzer"] = {
+        cargo = {
+          allFeatures = true,
+      },
+    },
+  },
+
+  --avante start
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+    opts = {
+      disabled_tools = { "python" },
+      api_keys = {
+        anthropic = vim.env.ANTHROPIC_API_KEY,
+        openai = vim.env.OPENAI_API_KEY,
+        gemini = vim.env.GEMINI_API_KEY,
+      }
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+}
